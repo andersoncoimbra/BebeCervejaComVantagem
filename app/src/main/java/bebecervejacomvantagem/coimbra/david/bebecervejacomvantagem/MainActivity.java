35aac40fe9cb8067f3242e10c6ffdf3381c10d30
Anderson Coimbra <andersonmegax@hotmail.com>
2015-08-06 12:43:30 -0300

    criação do LinearLayout, configurando os textBoc para receberem integer - double e interger, criação de tratamento de erros e coclusão do algoritmo de vereficação com tratamento de erro

--- app/src/main/java/bebecervejacomvantagem/coimbra/david/bebecervejacomvantagem/MainActivity.java
+++ app/src/main/java/bebecervejacomvantagem/coimbra/david/bebecervejacomvantagem/MainActivity.java
@@ -13,13 +13,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         final TextView mlCerveja;
-        TextView precoCErveja;
-        TextView teorCerveja;
+        final TextView precoCErveja;
+        final TextView teorCerveja;
        final TextView resultado;
         Button calcular;
 
         mlCerveja = (TextView) findViewById(R.id.mlCerveja);
+        precoCErveja = (TextView) findViewById(R.id.precoCerveja);
+        teorCerveja = (TextView) findViewById(R.id.teorCerveja);
         calcular = (Button) findViewById(R.id.calcular);
         resultado = (TextView) findViewById(R.id.resultado);
 
@@ -30,19 +34,41 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 try {
                     final Integer op = new Integer(mlCerveja.getText().toString());
+                    Double pr = new Double(precoCErveja.getText().toString());
+                    Integer teor = new Integer(teorCerveja.getText().toString());
+                    double result;
 
                     switch (op){
                         case 1:
+                            result = ((350 *teor)/pr);
+                            if(result >=350) {
+                                resultado.setText("1 - beba com moderacao " + result);
+                            }else{
+                                resultado.setText("Nao beba procure outro bar " + result);
+                            }
+
                         case 2:
+                            result = ((600 *teor)/pr);
+                            if(result >=300) {
+                                resultado.setText("2 - beba com moderamoderacao " + result);
+                            }else{
+                                resultado.setText("Nao beba procure outro bar " + result);
+                            }
+                            break;
                         case 3:
-                            resultado.setText("Tudo ok" + mlCerveja.getText());
+                            result = ((1000 *teor)/pr);
+                            if(result >=334) {
+                                resultado.setText("2 - beba com moderacao " + result);
+                            }else{
+                                resultado.setText("Nao beba procure outro bar " + result);
+                            }
                             break;
                         default:
-                            resultado.setText("Tudo errado" + mlCerveja.getText());
+                            resultado.setText("Escolha um opção de cerveja Valida" );
 
                     }
                 }catch (Exception e){
-                    resultado.setText("Tudo errado mesmo");
+                    resultado.setText("Algo deu errado... Tente novamente");
                 }
 
 
