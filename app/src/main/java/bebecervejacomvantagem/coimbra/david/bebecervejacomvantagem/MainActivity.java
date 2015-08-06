995b11be7ae5cb345eced1ff3a42f734f5fc9af7
Anderson Coimbra <andersonmegax@hotmail.com>
2015-08-06 03:17:27 -0300

    criação do LinearLayout, configurando os textBoc para receberem integer - double e interger, criação de tratamento de erros

--- app/src/main/java/bebecervejacomvantagem/coimbra/david/bebecervejacomvantagem/MainActivity.java
+++ app/src/main/java/bebecervejacomvantagem/coimbra/david/bebecervejacomvantagem/MainActivity.java
@@ -2,8 +2,6 @@ package bebecervejacomvantagem.coimbra.david.bebecervejacomvantagem;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,11 +13,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView mlCerveja;
+        final TextView mlCerveja;
         TextView precoCErveja;
         TextView teorCerveja;
-        final TextView resultado;
+       final TextView resultado;
         Button calcular;
 
         mlCerveja = (TextView) findViewById(R.id.mlCerveja);
@@ -27,15 +24,33 @@ public class MainActivity extends ActionBarActivity {
         resultado = (TextView) findViewById(R.id.resultado);
 
 
+
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resultado.setText("Tudo ok");
+                try {
+                    final Integer op = new Integer(mlCerveja.getText().toString());
+
+                    switch (op){
+                        case 1:
+                        case 2:
+                        case 3:
+                            resultado.setText("Tudo ok" + mlCerveja.getText());
+                            break;
+                        default:
+                            resultado.setText("Tudo errado" + mlCerveja.getText());
+
+                    }
+                }catch (Exception e){
+                    resultado.setText("Tudo errado mesmo");
+                }
+
 
 
             }
         });
     }
+
 
 
 
